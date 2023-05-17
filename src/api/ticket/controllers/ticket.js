db15dc9fecb2a41e7cc4cd7b995fda8b08e02305
batchild <batchayachivan1@gmail.com>
2023-05-17 20:29:12 +0100

    'Initial-pull'

---
+++ src/api/ticket/controllers/ticket.js
@@ -0,0 +1,23 @@
+'use strict';
+/**
+ *  ticket controller
+ */
+const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::ticket.ticket', ({ strapi }) => ({
+    async create(ctx) {
+        const event_id = Number(ctx.request.body.data.event)
+        // some logic here
+        const event = await strapi.service('api::event.event').findOne(event_id, {
+            populate: "tickets"
+        })
+        if(ctx.request.body.data.total_seats > event.tickets_available) {
+            return ctx.badRequest('Cannot book ticket at the moment')
+        }
+        const response = await strapi.service('api::ticket.ticket').create(ctx.request.body)
+        await strapi.service('api::event.event').update(event_id, { data: {
+            tickets_available: event.tickets_available - ctx.request.body.data.total_seats
+        }})
+        return response;
+      }
+      
+}));
